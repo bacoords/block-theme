e9c1f30b10f1090d66dee3570334d29e95a323b1
Brian Coords <bacoords@gmail.com>
2023-02-07 11:23:47 -0800

    adds basic theme setup from _s

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tangent functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Tangent
+ */
+
+namespace Tangent;
+
+/**
+ * Load theme setup.
+ */
+require_once get_template_directory() . '/inc/setup.php';
