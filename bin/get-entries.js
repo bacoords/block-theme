9de8c76280669bf247f3948a5550a2f4876b827e
Aurooba Ahmed <hey@aurooba.com>
2023-02-12 16:14:41 -0700

    modify getEntries function to be able to create block-specific stylesheet entries as well

--- bin/get-entries.js
+++ bin/get-entries.js
@@ -13,6 +13,7 @@ const path = require("path");
  * @param {string} root The root folder to search for files; should be relative to the root of the theme with no leading or trailing slashes
  * @param {string} include The glob pattern to use to find files
  * @param {string} outputFolder The folder to output the compiled files in; should be relative to the root of the theme with no leading or trailing slashes
+ * @param {bool} blockDir indicates if the sub directory structure should be used to create the output file name for block stylesheets
  * @returns
  */
 function getEntries(options) {
@@ -20,17 +21,34 @@ function getEntries(options) {
 		root = "src/scss",
 		include = "*.scss",
 		outputFolder = "css",
+		blockDir = false,
 	} = options;
 	// get all root scss files in the src/scss folder
 	const entries = glob.sync(root + "/" + include);
 
 	// create an object with the relative output path as the key and the file path as the value
 	const entriesWithKeys = entries.reduce((acc, entry) => {
-		const name = "../" + outputFolder + "/" + path.parse(entry).name;
-		acc[name] = path.resolve(entry);
+		// skip the index file
+		if ("index" === path.parse(entry).name) return acc;
+		const outputDir = "../" + outputFolder + "/";
+		const name = blockDir
+			? getBlockStylesheetName(entry)
+			: path.parse(entry).name;
+		acc[outputDir + name] = path.resolve(entry);
 		return acc;
 	}, {});
 	return entriesWithKeys;
+}
+
+function getBlockStylesheetName(filePath) {
+	const pathParts = filePath.split(path.sep);
+	if (pathParts.length) {
+		const fileName = pathParts[pathParts.length - 1];
+		pathParts[pathParts.length - 1] = path.parse(fileName).name;
+	}
+	const blockNamespace = pathParts[pathParts.length - 2];
+	const blockName = pathParts[pathParts.length - 1];
+	return blockNamespace + "-" + blockName;
 }
 
 // export the function so it can be used in the webpack.config.js file
