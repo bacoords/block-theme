53a7e7bf99f1731f17969e9e77a96c69b570c6ea
Aurooba Ahmed <hey@aurooba.com>
2023-02-12 22:44:01 -0700

    get all block css files in the css/ folder

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -30,3 +30,42 @@ function front_end_scripts() {
 	wp_enqueue_script( 'tangent-front-end-scripts', get_template_directory_uri() . '/js/scripts.js', $dependencies, $asset_file['version'], );
 }
 add_action( 'wp_enqueue_scripts', 'Tangent\Enqueue\front_end_scripts' );
+
+add_filter( 'should_load_separate_core_block_assets', '__return_true' );
+
+function enqueue_block_specific_custom_styles() {
+	/*
+	 * Load additional block styles.
+	 */
+		$styled_blocks = ['post-author'];
+	foreach ( $styled_blocks as $block_name ) {
+		$args = array(
+			'handle' => "twentytwentytwo-$block_name",
+			'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.css" ),
+		);
+		wp_enqueue_block_style( "core/$block_name", $args );
+	}
+}
+
+// function to get all css files in the css/ folder
+function get_css_files() {
+	$css_files = glob( get_template_directory() . '/css/*.css' );
+	$css_files = array_reduce( $css_files, 'Tangent\Enqueue\css_file_keys', array() );
+	$css_files = array_filter( $css_files, 'Tangent\Enqueue\exclude_stylesheets', ARRAY_FILTER_USE_KEY );
+	echo '<pre>' . var_export($css_files, true) . '</pre>';
+
+	return $css_files;
+}
+
+function css_file_keys( $accumulator, $css_file ) {
+	$accumulator[basename( $css_file )] = $css_file;
+	return $accumulator;
+}
+
+function exclude_stylesheets( $stylesheet ) {
+	$exclude_stylesheets = array( 'global.css', 'editor.css' );
+	if ( in_array( $stylesheet, $exclude_stylesheets, true ) ) {
+		return false;
+	}
+	return true;
+}
