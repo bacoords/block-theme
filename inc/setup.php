509016ab6d2f9796154bb2f1444bc9e2db9e55f7
Aurooba Ahmed <hey@aurooba.com>
2023-02-21 17:47:52 -0700

    adds a commented out registration of a sidebar

--- inc/setup.php
+++ inc/setup.php
@@ -93,17 +93,17 @@ function theme_setup() {
 	/**
 	 * Uncomment the following block of code to register a sidebar.
 	 */
-	// register_sidebar(
-	// 	array(
-	// 		'name'          => esc_html__( 'Sidebar', 'tangent' ),
-	// 		'id'            => 'sidebar-1',
-	// 		'description'   => esc_html__( 'Add widgets here.', 'tangent' ),
-	// 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-	// 		'after_widget'  => '</section>',
-	// 		'before_title'  => '<h2 class="widget-title">',
-	// 		'after_title'   => '</h2>',
-	// 	)
-	// );
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'tangent' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'tangent' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action( 'after_setup_theme', 'Tangent\Setup\theme_setup' );
 
