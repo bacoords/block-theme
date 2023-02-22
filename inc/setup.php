6faa2ce3cce5299ac922db0855a9d11141025640
Aurooba Ahmed <hey@aurooba.com>
2023-02-21 17:54:17 -0700

    add extra asterisk to docblocks in setup

--- inc/setup.php
+++ inc/setup.php
@@ -11,7 +11,7 @@ namespace Tangent\Setup;
  * Setup theme
  */
 function theme_setup() {
-	/*
+	/**
 	* Make theme available for translation.
 	* Translations can be filed in the /languages/ directory.
 	* If you're building a theme based on _s, use a find and replace
@@ -22,7 +22,7 @@ function theme_setup() {
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
-	/*
+	/**
 	* Let WordPress manage the document title.
 	* By adding theme support, we declare that this theme does not use a
 	* hard-coded <title> tag in the document head, and expect WordPress to
@@ -30,7 +30,7 @@ function theme_setup() {
 	*/
 	add_theme_support( 'title-tag' );
 
-	/*
+	/**
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
@@ -59,7 +59,7 @@ function theme_setup() {
 		)
 	);
 
-	/*
+	/**
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
 	 */
