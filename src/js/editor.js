0f1dab700f41f61e27d9bab8803fdf1aa385e46d
Brian Coords <bacoords@gmail.com>
2023-04-12 07:20:45 -0700

    comments out columns block variation

--- src/js/editor.js
+++ src/js/editor.js
@@ -76,17 +76,17 @@ const unregisterBlockVariations = {
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations/
  */
 const registerBlockVariations = {
-	"core/columns": {
-		name: "three-columns-wide-left",
-		title: "50 / 25 / 25",
-		description: "Three columns; wide left column",
-		innerBlocks: [
-			["core/column", { width: "50%" }],
-			["core/column", { width: "25%" }],
-			["core/column", { width: "25%" }],
-		],
-		scope: ["block"],
-	},
+	// "core/columns": {
+	// 	name: "three-columns-wide-left",
+	// 	title: "50 / 25 / 25",
+	// 	description: "Three columns; wide left column",
+	// 	innerBlocks: [
+	// 		["core/column", { width: "50%" }],
+	// 		["core/column", { width: "25%" }],
+	// 		["core/column", { width: "25%" }],
+	// 	],
+	// 	scope: ["block"],
+	// },
 };
 
 /**
