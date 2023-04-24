5f5d179fc82bfbe31232bd057c5eedee22a841f2
Brian Coords <bacoords@gmail.com>
2023-04-24 10:29:09 -0700

    removes siteinfo div

--- footer.php
+++ footer.php
@@ -12,10 +12,7 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-
-		<div class="site-info">
-			<?php block_template_part( 'footer-credit' ); ?>
-		</div>
+		<?php block_template_part( 'footer-credit' ); ?>
 	</footer>
 </div>
 
