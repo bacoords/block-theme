3ef099f0638bae935c6ca2acca20320f75281175
Aurooba Ahmed <hey@aurooba.com>
2023-05-31 17:31:22 -0600

    move conditional so no nav at all is rendered

--- template-parts/navigation-offcanvas.php
+++ template-parts/navigation-offcanvas.php
@@ -6,7 +6,10 @@
  */
 
 ?>
-
+<?php
+// don't render the navigation if no menu is assigned.
+if ( has_nav_menu( 'menu-1' ) ) {
+	?>
 <nav id="site-navigation" class="offcanvas-navigation">
 
 	<button class="menu-toggle link-button" aria-haspopup="true" data-micromodal-trigger="navigation-modal" aria-label="<?php esc_html_e( 'Primary Menu', 'tangent' ); ?>">
@@ -26,7 +29,6 @@
 				</header>
 
 				<?php
-				if ( has_nav_menu( 'menu-1' ) ) {
 					wp_nav_menu(
 						array(
 							'theme_location'  => 'menu-1',
@@ -37,10 +39,10 @@
 							'walker'          => new Tangent\Navwalker\Tangent_Navwalker(),
 						)
 					);
-				}
 				?>
 
 			</div>
 		</div>
 	</div>
 </nav>
+<?php } ?>
